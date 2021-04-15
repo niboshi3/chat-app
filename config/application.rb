d90351cc427baba42629cd3caee48aae06118910
niboshi3 <nib0shi.vs.nek0n0c0@gmail.com>
2021-04-15 14:55:32 +0900

    サイドバーのフロント実装

--- config/application.rb
+++ config/application.rb
@@ -16,10 +16,10 @@ module ChatApp
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.generators do |g|
-      g.stylesheets falase
-      g.javascripts falase
-      g.helper falase
-      g.test_framework falase
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
     end
   end
 end
